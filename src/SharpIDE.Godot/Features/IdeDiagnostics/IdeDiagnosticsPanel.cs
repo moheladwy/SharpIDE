b7029e7dce3b5b325f256764584d46e44e69ea61
Matt Parker <61717342+MattParkerDev@users.noreply.github.com>
2025-09-26 23:47:25 +1000

    add v1 loading graph

--- src/SharpIDE.Godot/Features/IdeDiagnostics/IdeDiagnosticsPanel.cs
+++ src/SharpIDE.Godot/Features/IdeDiagnostics/IdeDiagnosticsPanel.cs
@@ -4,5 +4,16 @@ namespace SharpIDE.Godot.Features.IdeDiagnostics;
 
 public partial class IdeDiagnosticsPanel : Control
 {
-    
+    private GraphEdit _graphEdit = null!;
+    public override void _Ready()
+    {
+        _graphEdit = GetNode<GraphEdit>("%GraphEdit");
+        //_graphEdit.ConnectionRequest += GraphEditOnConnectionRequest;
+    }
+
+    // private void GraphEditOnConnectionRequest(StringName fromNode, long fromPort, StringName toNode, long toPort)
+    // {
+    //     GD.Print($"Connection requested from {fromNode} port {fromPort} to {toNode} port {toPort}");
+    //     _graphEdit.ConnectNode(fromNode, (int)fromPort, toNode, (int)toPort);
+    // }
 }
