d58562843aecaf88ed00667adb0c38ea89f490a6
Matt Parker <61717342+MattParkerDev@users.noreply.github.com>
2025-10-28 22:29:26 +1000

    Update SharpIdeCodeEdit.cs

--- src/SharpIDE.Godot/Features/CodeEditor/SharpIdeCodeEdit.cs
+++ src/SharpIDE.Godot/Features/CodeEditor/SharpIdeCodeEdit.cs
@@ -64,7 +64,7 @@ public partial class SharpIdeCodeEdit : CodeEdit
 	[
 		//"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",
 		//"0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
-	    "(", ",", "=", "\t"
+	    "(", ",", "=", "\t", ":"
 	];
 
 	public override void _Ready()
